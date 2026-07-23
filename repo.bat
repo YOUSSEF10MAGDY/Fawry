d9c37e4eb89b912482746c54af5fa0f560827f95
YOUSSEF10MAGDY <youssef1267yg@gmail.com>
2026-07-24 00:19:43 +0300

    Initial commit

--- repo.bat
+++ repo.bat
@@ -6,7 +6,7 @@ git commit -m "Initial commit"
 
 git branch -M main
 
-git remote add origin https://github.com/YOUSSEF10MAGDY/Fawry-internship.git
+git remote add origin git@github.com:YOUSSEF10MAGDY/Fawry-internship.git
 
 git push -u origin main
 pause
